c0b57d89789ed68207512610c04fe4036ecb1005
user <71089904+Anton-Lapanovich@users.noreply.github.com>
2022-06-12 03:37:57 +0300

    Add files via upload

---
+++ OOPLab_4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using DependencyInjectionContainer.DependencyInjection;
+
+namespace DependencyInjectionContainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new DiCollection();
+
+            services.RegisterTransient<IProvider, RandomGuidProvider>();
+            services.RegisterSingleton<IService, Service>();
+            
+            
+            var container = services.GenerateContainer();
+
+            var service = container.GetService<IProvider>();
+            var serviceFirst = container.GetService<IService>();
+            var serviceSecond = container.GetService<IService>();
+            var service3 = container.GetService<IProvider>();
+            
+            serviceFirst.PrintSomething();
+            serviceSecond.PrintSomething();
+            Console.WriteLine(service.RandomGuid);
+            Console.WriteLine(service3.RandomGuid);
+        }
+    }
+}
